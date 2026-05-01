e05548f32e3941ee9757aa68ed2f8f4614d412cb
Jakob Wandel <sonnentau.koenig@gmail.com>
2026-05-02 01:04:08 +0200

    add color lib as submodule, working example

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,18 @@
 #include <Arduino.h>
 
+#include <color/color.hpp>
+
 void setup() {
   Serial.begin(9600);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
 void loop() {
+  color::RGB<int, 3U> color{255, 55, 0};
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
+  delay(color.r() * 4);
   Serial.println("off");
   digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  delay(color.g() * 4);
   Serial.println("on");
 }
