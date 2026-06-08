abd51b1f202bc434c8e4f2b875db0d7ff1a752a6
Jakob Wandel <sonnentau.koenig@gmail.com>
2026-06-08 22:05:59 +0200

    removed unused include

--- lib/Enums/include/Enums/Enums.hpp
+++ lib/Enums/include/Enums/Enums.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
-#include <ExpandEnums.hpp>
+#include <cstdint>
 
-enum class WIFI_STATUS { DISCONNECTED, CONNECTED, CONNECTING };
+enum class WIFI_STATUS : std::uint8_t { DISCONNECTED, CONNECTED, CONNECTING };
 
-enum class HASP_STATUS { DISCONNECTED, CONNECTED, CONNECTING };
+enum class HASP_STATUS : std::uint8_t { DISCONNECTED, CONNECTED, CONNECTING };
